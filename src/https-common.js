5b91d915c93ff12b38ab3a75c470e93562ec8134
DeepAlgoExpert <beaumarfdeep0821@gmail.com>
2024-05-31 08:34:56 +0300

    common

--- src/https-common.js
+++ src/https-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://54.83.136.252",
+  baseURL: "https://100.25.156.233",
   //baseURL: "http://localhost:5000",
   headers: {
     "Content-type": "application/json"
